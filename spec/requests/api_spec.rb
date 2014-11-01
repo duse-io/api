12bd6fcd131ec224eb708a45ea80709b4df644a4
flower-pot <flower-pot@flowerpot.io>
2014-11-01 14:07:03 +0100

    refactor api tests

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -8,25 +8,32 @@ describe API do
     API::API
   end
 
-  it 'persists a new secret correctly' do
+  def secret(options = {})
+    {
+      title: options[:title] || 'my secret',
+      required: options[:required] || 2,
+      split: options[:split] || 4,
+      parts: options[:parts] || [
+        {"#{1}" => "1-19810ad8", "#{2}" => "2-2867e0bd", "#{3}" => "3-374eb6a2"},
+        {"#{1}" => "1-940cc79",  "#{2}" => "2-e671f52",  "#{3}" => "3-138d722b"},
+        {"#{1}" => "1-3e8f8a59", "#{2}" => "2-70f6da4d", "#{3}" => "3-235e2a42"},
+        {"#{1}" => "1-117c3",    "#{2}" => "2-1f592",    "#{3}" => "3-d362"}
+      ]
+    }
+  end
+
+  def create_users!
     server = User.create({username: 'server', api_key: 'test123'})
     adracus = User.create({username: 'adracus', api_key: 'test123'})
     flowerpot = User.create({username: 'flower-pot', api_key: 'test123'})
-    users = [server, adracus, flowerpot]
+    [server, adracus, flowerpot]
+  end
 
-    secret = {
-      title: 'my secret',
-      required: 2,
-      split: 4,
-      parts: [
-        {"#{server.id}" => "1-19810ad8", "#{adracus.id}" => "2-2867e0bd", "#{flowerpot.id}" => "3-374eb6a2"},
-        {"#{server.id}" => "1-940cc79",  "#{adracus.id}" => "2-e671f52",  "#{flowerpot.id}" => "3-138d722b"},
-        {"#{server.id}" => "1-3e8f8a59", "#{adracus.id}" => "2-70f6da4d", "#{flowerpot.id}" => "3-235e2a42"},
-        {"#{server.id}" => "1-117c3",    "#{adracus.id}" => "2-1f592",    "#{flowerpot.id}" => "3-d362"}
-      ]
-    }.to_json
+  it 'persists a new secret correctly' do
+    users = create_users!
+    secret_json = secret.to_json
 
-    post '/v1/secrets', secret, 'CONTENT_TYPE' => 'application/json'
+    post '/v1/secrets', secret_json, 'CONTENT_TYPE' => 'application/json'
 
     expect(last_response.status).to eq(201)
     expect(User.all.count).to eq(3)
@@ -36,7 +43,15 @@ describe API do
   end
 
   it 'should error when title is empty' do
+    secret_json = secret(title: '').to_json
 
+    post '/v1/secrets', secret.to_json, 'CONTENT_TYPE' => 'application/json'
+
+    expect(last_response.status).to eq(422)
+    expect(User.all.count).to eq(0)
+    expect(Secret.all.count).to eq(0)
+    expect(SecretPart.all.count).to eq(0)
+    expect(Share.all.count).to eq(0)
   end
 
   it 'should only accept required >= 2' do

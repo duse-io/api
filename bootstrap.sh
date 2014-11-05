42eec9e98c524340d7246ecb35cd6309b3817100
flower-pot <flower-pot@flowerpot.io>
2014-11-05 12:27:59 +0100

    add vagrant file and setup shell script

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update -q -y
+
+sudo apt-get -y --quiet install postgresql postgresql-contrib libpq-dev postgresql-server-dev-9.3
+
+su -l vagrant -c "gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
+su -l vagrant -c "curl -L https://get.rvm.io | bash -s stable"
+su -l vagrant -c "source \"/home/vagrant/.rvm/scripts/rvm\""
+su -l vagrant -c "rvm install ruby-2.1.3"
+su -l vagrant -c "gem install bundler"
+
+sudo -u postgres psql -U postgres -d postgres -c "CREATE USER duse_api WITH PASSWORD 'password1' CREATEDB;"
+sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE duse_api_test WITH OWNER duse_api;"

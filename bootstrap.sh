37a368fd34eeccba44b649b66ba36a1d0e071186
flower-pot <flower-pot@flowerpot.io>
2014-11-06 10:18:46 +0100

    install correct ruby version

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo apt-get -y --quiet install postgresql postgresql-contrib libpq-dev postgres
 su -l vagrant -c "gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
 su -l vagrant -c "curl -L https://get.rvm.io | bash -s stable"
 su -l vagrant -c "source \"/home/vagrant/.rvm/scripts/rvm\""
-su -l vagrant -c "rvm install ruby-2.1.3"
+su -l vagrant -c "rvm install ruby-2.1.4"
 su -l vagrant -c "gem install bundler"
 
 sudo -u postgres psql -U postgres -d postgres -c "CREATE USER duse_api WITH PASSWORD 'password1' CREATEDB;"

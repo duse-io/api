64bceb2eddd0d1edd98f067db073ee9d1ee410db
flower-pot <flower-pot@flowerpot.io>
2014-11-13 17:51:29 +0100

    don't try to create server user when migrating

--- config.ru
+++ config.ru
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
+if Model::User.first(username: 'server').nil?
+  user = Model::User.create(username: 'server', api_token: 'irsndnafdnwfndnw', password: 'rstnioerndordnior')
+end
+
 require 'api'
 run API::API
--- config/database.rb
+++ config/database.rb
@@ -5,7 +5,3 @@ require 'models/secret_part'
 require 'models/share'
 DataMapper::Model.raise_on_save_failure = true
 DataMapper.finalize
-
-if Model::User.first(username: 'server').nil?
-  user = Model::User.create(username: 'server', api_token: 'irsndnafdnwfndnw', password: 'rstnioerndordnior')
-end

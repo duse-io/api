564bdb7b5fa309f5aad5e9e97060d66e2dbfd5ab
flower-pot <flower-pot@flowerpot.io>
2014-11-28 15:02:17 +0100

    stronger username and password validations

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -9,10 +9,10 @@ class User
   before :create, :set_new_token
 
   property :id,         Serial
-  property :username,   String,     required: true, index: true
+  property :username,   String,     required: true, index: true, unique: true
   property :password,   BCryptHash, required: true
   attr_accessor :password_confirmation
-  property :api_token,  String,     index: true
+  property :api_token,  String,     index: true, unique: true
   property :public_key, PublicKey,  required: true
   property :private_key,Text
 
@@ -21,8 +21,8 @@ class User
   validates_with_method :public_key, method: :validate_public_key
   validates_with_method :password_confirmation, method: :validate_password_complexity, if: :new?
   validates_with_method :password_confirmation, method: :validate_password_equalness, if: :new?
-  validates_length_of   :password_confirmation, min: 8, if: :new?
-  validates_format_of   :username, with: /[a-zA-Z0-9_-]{4,30}$/
+  validates_length_of   :password_confirmation, min: 8, if: :new?, message: 'Password must be at least 8 characters long'
+  validates_format_of   :username, with: /[a-zA-Z0-9_-]{4,30}$/, message: 'Username must be only letters, capital letters, numbers, "-" and "_". And at least 4 characters long.'
 
   def set_new_token
     self.api_token = generate_save_token
@@ -36,7 +36,7 @@ class User
   end
 
   def validate_password_complexity
-    if (/.*[[:punct:]]+.*/ =~ self.password_confirmation).nil? || # includes punctuation
+    if (/.*[[:punct:]\W]+.*/ =~ self.password_confirmation).nil? || # includes punctuation/special chars
        (/.*[[:upper:]]+.*/ =~ self.password_confirmation).nil? || # includes uppercase letters
        (/.*[[:lower:]]+.*/ =~ self.password_confirmation).nil? || # includes lowercase letters
        (/.*\d+.*/          =~ self.password_confirmation).nil?    # includes digits letters

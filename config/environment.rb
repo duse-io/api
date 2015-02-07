6a19d4612bb8ddfd4ea0c871b384d57d62f342c1
flower-pot <flower-pot@flowerpot.io>
2015-02-07 17:09:31 +0100

    no need to set the env manually
    rackup automatically does that

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require 'bundler/setup'
 require 'dotenv'
 Dotenv.load
 
-ENV['ENV'] ||= 'development'
-
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'api'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 Bundler.require

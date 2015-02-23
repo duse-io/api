824033519b78329e2659ff4bef79ce7ed5eb04a1
flower-pot <flower-pot@flowerpot.io>
2015-02-23 16:34:26 +0100

    only configure raven when sentry dsn is set

--- config/sentry.rb
+++ config/sentry.rb
@@ -1,7 +1,9 @@
 require 'duse'
 require 'raven'
 
-Raven.configure do |config|
-  config.dsn = Duse.config.sentry_dsn
+if Duse.config.use_sentry?
+  Raven.configure do |config|
+    config.dsn = Duse.config.sentry_dsn
+  end
 end
 

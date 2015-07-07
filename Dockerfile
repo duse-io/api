cc20063943c93e54356ff748ac10125bbb2387aa
flower-pot <fbranczyk@gmail.com>
2015-07-07 17:09:43 +0200

    wait for postgres to start before starting up

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ruby:2.2.2
 
 RUN apt-get update -qq
-RUN apt-get install -y build-essential libpq-dev
+RUN apt-get install -y build-essential libpq-dev postgresql-client-9.4
 
 # throw errors if Gemfile has been modified since Gemfile.lock
 RUN bundle config --global frozen 1
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 set -e
 
+HOST_PORT=`echo $DATABASE_URL | awk -F/ '{print $3}' | awk -F@ '{print $2}'`
+HOST=`echo $HOST_PORT | awk -F: '{print $1}'`
+PORT=`echo $HOST_PORT | awk -F: '{print $2}'`
+
+if [ -z "${PORT}" ]; then
+  PORT='5432'
+fi
+
+ping_postgres_successful()
+{
+  pg_isready --host=$HOST > /dev/null 2>&1
+  return $?
+}
+
+until ( ping_postgres_successful ); do
+  sleep 1s
+  echo 'Waiting for postgres to start ...'
+done
+
 bundle exec rake db:migrate
 bundle exec bundle exec rackup -p 5000 --host 0.0.0.0

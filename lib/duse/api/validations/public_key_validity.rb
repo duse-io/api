47b595b2eedecdbd26e92e9f7bc40d60844b5b77
flower-pot <fbranczyk@gmail.com>
2015-07-27 21:25:42 +0200

    split key validity and size validations

---
+++ lib/duse/api/validations/public_key_validity.rb
@@ -0,0 +1,27 @@
+require 'duse/api/validations/single'
+
+module Duse
+  module API
+    module Validations
+      class PublicKeyValidity
+        include Single
+
+        def invalid?(public_key)
+          !public_key.nil? && !is_valid?(public_key)
+        end
+
+        def error_msg
+          'Public key is not a valid RSA Public Key'
+        end
+
+        def is_valid?(public_key)
+          key = OpenSSL::PKey::RSA.new public_key
+          key.public?
+        rescue OpenSSL::PKey::RSAError
+          false
+        end
+      end
+    end
+  end
+end
+
--- lib/duse/api/validations/user.rb
+++ lib/duse/api/validations/user.rb
@@ -4,7 +4,8 @@ require 'duse/api/validations/format'
 require 'duse/api/validations/max_length'
 require 'duse/api/validations/length_between'
 require 'duse/api/validations/password_complexity'
-require 'duse/api/validations/public_key_validity_and_size'
+require 'duse/api/validations/public_key_validity'
+require 'duse/api/validations/public_key_size'
 
 module Duse
   module API
@@ -26,7 +27,8 @@ module Duse
         end
 
         class PublicKey < Multi
-          validate PublicKeyValidityAndSize
+          validate PublicKeyValidity
+          validate PublicKeySize
         end
 
         validate Password, :password, on: :create

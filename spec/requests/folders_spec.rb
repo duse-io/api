29105eee93ad17107e43ee340d7bf4b1f4ed4d6e
Frederic Branczyk <fbranczyk@gmail.com>
2015-10-22 17:48:39 +0200

    improve use of rspec and refactor folder specs

--- spec/requests/folders_spec.rb
+++ spec/requests/folders_spec.rb
@@ -5,218 +5,232 @@ RSpec.describe Duse::API, type: :request do
     Duse::API::App.new
   end
 
-  before :each do
-    @user = FactoryGirl.create(:user)
-  end
+  let!(:user) { FactoryGirl.create(:user) }
 
   describe "POST /folders" do
-    it "responds with 201 when creating" do
-      folder_json = default_folder.to_json
+    context "when provided attributes are valid" do
+      it "responds with 201 when creating" do
+        folder_json = default_folder.to_json
 
-      header "Authorization", @user.create_new_token
-      post "/v1/folders", folder_json, "CONTENT_TYPE" => "application/json"
-
-      expect(last_response.status).to eq(201)
-    end
-
-    it "persists a new folder correctly" do
-      folder_json = default_folder.to_json
-
-      header "Authorization", @user.create_new_token
-      expect {
+        header "Authorization", user.create_new_token
         post "/v1/folders", folder_json, "CONTENT_TYPE" => "application/json"
-      }.to change{ Duse::API::Models::Folder.count }.by(1)
+
+        expect(last_response.status).to eq(201)
+      end
+
+      it "persists a new folder correctly" do
+        folder_json = default_folder.to_json
+
+        header "Authorization", user.create_new_token
+        expect {
+          post "/v1/folders", folder_json, "CONTENT_TYPE" => "application/json"
+        }.to change{ Duse::API::Models::Folder.count }.by(1)
+      end
     end
 
-    it "errors when the folder contains illegal characters" do
-      folder_json = { name: "???" }.to_json
+    context "when provided attributes are invalid" do
+      it "validates and errors" do
+        folder_json = { name: "???" }.to_json
 
-      header "Authorization", @user.create_new_token
-      post "/v1/folders", folder_json, "CONTENT_TYPE" => "application/json"
+        header "Authorization", user.create_new_token
+        post "/v1/folders", folder_json, "CONTENT_TYPE" => "application/json"
 
-      expect(last_response.status).to eq(422)
+        expect(last_response.status).to eq(422)
+      end
     end
   end
 
   describe "GET /folders/:id" do
-    it "returns ok when getting" do
-      folder  = FactoryGirl.create(:folder, user: @user)
+    let!(:folder) { FactoryGirl.create(:folder, user: user) }
+    let!(:inner_folder) { FactoryGirl.create(:folder, user: user, name: "innerFolder", parent: folder) }
 
-      header "Authorization", @user.create_new_token
+    it "returns ok when getting" do
+      header "Authorization", user.create_new_token
       get "/v1/folders/#{folder.id}", "CONTENT_TYPE" => "application/json"
 
       expect(last_response.status).to eq 200
     end
 
     it "nests folders when rendering" do
-      first  = FactoryGirl.create(:folder, user: @user)
-      second = FactoryGirl.create(:folder, user: @user, name: "innerFolder", parent: first)
-
-      header "Authorization", @user.create_new_token
-      get "/v1/folders/#{first.id}", "CONTENT_TYPE" => "application/json"
+      header "Authorization", user.create_new_token
+      get "/v1/folders/#{folder.id}", "CONTENT_TYPE" => "application/json"
 
       expect(last_response.body).to eq({
-        id: first.id,
+        id: folder.id,
         name: "testFolder",
         subfolders: [
           {
-            id: second.id,
+            id: inner_folder.id,
             name: "innerFolder",
             subfolders: [],
             secrets: [],
-            url: "http://example.org/v1/folders/#{second.id}"
+            url: "http://example.org/v1/folders/#{inner_folder.id}"
           }
         ],
         secrets: [],
-        url: "http://example.org/v1/folders/#{first.id}"
+        url: "http://example.org/v1/folders/#{folder.id}"
       }.to_json)
     end
   end
 
   describe "GET /folders" do
     it "returns ok" do
-      header "Authorization", @user.create_new_token
+      header "Authorization", user.create_new_token
       get "/v1/folders", "CONTENT_TYPE" => "application/json"
 
       expect(last_response.status).to eq 200
     end
 
-    it "returns a default folder named like the user" do
-      header "Authorization", @user.create_new_token
-      get "/v1/folders", "CONTENT_TYPE" => "application/json"
+    context "when no folders exist" do
+      it "returns a default folder named like the user" do
+        header "Authorization", user.create_new_token
+        get "/v1/folders", "CONTENT_TYPE" => "application/json"
 
-      expect(last_response.body).to eq([{
-        id: nil,
-        name: @user.username,
-        subfolders: [],
-        secrets: []
-      }].to_json)
-    end
-
-    it "returns a default folder containing all other folders owned by the user" do
-      folder = FactoryGirl.create(:folder, user: @user)
-
-      header "Authorization", @user.create_new_token
-      get "/v1/folders", "CONTENT_TYPE" => "application/json"
-
-      expect(last_response.body).to eq([{
-        id: nil,
-        name: @user.username,
-        subfolders: [{
-          id: folder.id,
-          name: "testFolder",
+        expect(last_response.body).to eq([{
+          id: nil,
+          name: user.username,
           subfolders: [],
-          secrets: [],
-          url: "http://example.org/v1/folders/#{folder.id}"
-        }],
-        secrets: []
-      }].to_json)
+          secrets: []
+        }].to_json)
+      end
     end
 
-    it "puts secrets without a folder in the default folder" do
-      secret = FactoryGirl.create(:secret, users: [@user])
+    context "when folders exist" do
+      let!(:folder) { FactoryGirl.create(:folder, user: user) }
 
-      header "Authorization", @user.create_new_token
-      get "/v1/folders", "CONTENT_TYPE" => "application/json"
+      it "returns a default folder containing all other folders owned by the user" do
+        header "Authorization", user.create_new_token
+        get "/v1/folders", "CONTENT_TYPE" => "application/json"
 
-      expect(last_response.body).to eq([{
-        id: nil,
-        name: @user.username,
-        subfolders: [],
-        secrets: [{
-          id: secret.id,
-          title: secret.title,
-          url: "http://example.org/v1/secrets/#{secret.id}"
-        }]
-      }].to_json)
+        expect(last_response.body).to eq([{
+          id: nil,
+          name: user.username,
+          subfolders: [{
+            id: folder.id,
+            name: "testFolder",
+            subfolders: [],
+            secrets: [],
+            url: "http://example.org/v1/folders/#{folder.id}"
+          }],
+          secrets: []
+        }].to_json)
+      end
     end
 
-    it "correctly nests secrets" do
-      folder = FactoryGirl.create(:folder, user: @user)
-      secret = FactoryGirl.create(:secret, users: [@user])
-      secret.user_secrets.where(user: @user).first.update(folder_id: folder.id)
+    context "when a secret is not in a folder" do
+      let!(:secret) { FactoryGirl.create(:secret, users: [user]) }
 
-      header "Authorization", @user.create_new_token
-      get "/v1/folders", "CONTENT_TYPE" => "application/json"
+      it "puts the secret in the default folder" do
+        header "Authorization", user.create_new_token
+        get "/v1/folders", "CONTENT_TYPE" => "application/json"
 
-      expect(last_response.body).to eq([{
-        id: nil,
-        name: @user.username,
-        subfolders: [{
-          id: folder.id,
-          name: "testFolder",
+        expect(last_response.body).to eq([{
+          id: nil,
+          name: user.username,
           subfolders: [],
           secrets: [{
             id: secret.id,
             title: secret.title,
             url: "http://example.org/v1/secrets/#{secret.id}"
-          }],
-          url: "http://example.org/v1/folders/#{folder.id}"
-        }],
-        secrets: []
-      }].to_json)
+          }]
+        }].to_json)
+      end
     end
 
-    it "puts a secret in the default folder if the folder it lies in is deleted" do
-      folder = FactoryGirl.create(:folder, user: @user)
-      secret = FactoryGirl.create(:secret, users: [@user])
-      secret.user_secrets.where(user: @user).first.update(folder_id: folder.id)
-      folder.destroy
+    context "when a secret is in a folder" do
+      let!(:folder) {
+        folder = FactoryGirl.create(:folder, user: user)
+      }
+      let!(:secret) do
+        secret = FactoryGirl.create(:secret, users: [user])
+        secret.user_secrets.where(user: user).first.update(folder_id: folder.id)
+        secret
+      end
 
-      header "Authorization", @user.create_new_token
-      get "/v1/folders", "CONTENT_TYPE" => "application/json"
+      it "correctly nests the secret" do
+        header "Authorization", user.create_new_token
+        get "/v1/folders", "CONTENT_TYPE" => "application/json"
 
-      expect(last_response.body).to eq([{
-        id: nil,
-        name: @user.username,
-        subfolders: [],
-        secrets: [{
-          id: secret.id,
-          title: secret.title,
-          url: "http://example.org/v1/secrets/#{secret.id}"
-        }]
-      }].to_json)
+        expect(last_response.body).to eq([{
+          id: nil,
+          name: user.username,
+          subfolders: [{
+            id: folder.id,
+            name: "testFolder",
+            subfolders: [],
+            secrets: [{
+              id: secret.id,
+              title: secret.title,
+              url: "http://example.org/v1/secrets/#{secret.id}"
+            }],
+            url: "http://example.org/v1/folders/#{folder.id}"
+          }],
+          secrets: []
+        }].to_json)
+      end
+
+      it "puts a secret in the default folder if the folder it lies in is deleted" do
+        folder.destroy
+
+        header "Authorization", user.create_new_token
+        get "/v1/folders", "CONTENT_TYPE" => "application/json"
+
+        expect(last_response.body).to eq([{
+          id: nil,
+          name: user.username,
+          subfolders: [],
+          secrets: [{
+            id: secret.id,
+            title: secret.title,
+            url: "http://example.org/v1/secrets/#{secret.id}"
+          }]
+        }].to_json)
+      end
     end
   end
 
   describe "PUT/PATCH /folders/:id" do
-    it "returns ok" do
-      folder = FactoryGirl.create(:folder, user: @user)
+    context "provided attributes are valid" do
+      it "returns ok" do
+        folder = FactoryGirl.create(:folder, user: user)
 
-      header "Authorization", @user.create_new_token
-      patch "/v1/folders/#{folder.id}", { name: "newFolderName" }.to_json, "CONTENT_TYPE" => "application/json"
+        header "Authorization", user.create_new_token
+        patch "/v1/folders/#{folder.id}", { name: "newFolderName" }.to_json, "CONTENT_TYPE" => "application/json"
 
-      expect(last_response.status).to eq 200
-    end
+        expect(last_response.status).to eq 200
+      end
 
-    it "updates the folders name" do
-      folder = FactoryGirl.create(:folder, user: @user)
+      it "updates the folders name" do
+        folder = FactoryGirl.create(:folder, user: user)
 
-      header "Authorization", @user.create_new_token
-      patch "/v1/folders/#{folder.id}", { name: "newFolderName" }.to_json, "CONTENT_TYPE" => "application/json"
+        header "Authorization", user.create_new_token
+        patch "/v1/folders/#{folder.id}", { name: "newFolderName" }.to_json, "CONTENT_TYPE" => "application/json"
 
-      folder.reload
-      expect(folder.name).to eq "newFolderName"
+        folder.reload
+        expect(folder.name).to eq "newFolderName"
+      end
     end
   end
 
   describe "DELETE /folders/:id" do
-    it "returns no content" do
-      folder = FactoryGirl.create(:folder, user: @user)
+    context "when the folder exists and the user has the right to delete it" do
+      it "returns no content" do
+        folder = FactoryGirl.create(:folder, user: user)
 
-      header "Authorization", @user.create_new_token
-      delete "/v1/folders/#{folder.id}", { name: "newFolderName" }.to_json, "CONTENT_TYPE" => "application/json"
-
-      expect(last_response.status).to eq 204
-    end
-
-    it "deletes the folder" do
-      folder = FactoryGirl.create(:folder, user: @user)
-
-      expect {
-        header "Authorization", @user.create_new_token
+        header "Authorization", user.create_new_token
         delete "/v1/folders/#{folder.id}", { name: "newFolderName" }.to_json, "CONTENT_TYPE" => "application/json"
-      }.to change{ Duse::API::Models::Folder.count }.by(-1)
+
+        expect(last_response.status).to eq 204
+      end
+
+      it "deletes the folder" do
+        folder = FactoryGirl.create(:folder, user: user)
+
+        expect {
+          header "Authorization", user.create_new_token
+          delete "/v1/folders/#{folder.id}", { name: "newFolderName" }.to_json, "CONTENT_TYPE" => "application/json"
+        }.to change{ Duse::API::Models::Folder.count }.by(-1)
+      end
     end
   end
 end
